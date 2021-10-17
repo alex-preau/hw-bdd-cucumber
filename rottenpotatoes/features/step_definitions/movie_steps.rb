52b16804965c9a04650d3b2b60254a019380fe38
Alexander Preau <awp2128@columbia.edu>
2021-10-17 23:14:20 +0000

    done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,15 @@
 # Add a declarative step here for populating the DB with movies.
 
+
+
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+      Movie.create(title:movie[:title], rating: movie[:rating], release_date:movie[:release_date])
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+ # fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +22,13 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  all_page = page.body 
+  index_1 = all_page.index(e1)
+  index_2 = all_page.index(e2)
+  if index_1 > index_2
+      fail 
+  end
+ # assert index_1 < index_2
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,13 +36,32 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+    #print(uncheck)
+    rating_list.split(',') do |rating| 
+        if not uncheck
+            check('ratings_' + rating.lstrip)
+        else 
+            uncheck('ratings_' + rating.lstrip)
+        end
+    end
+
+
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  all_movies = Movie.all
+    
+  all_movies.each do |m| 
+      page.should have_content(m.title)
+  end
 end
+
+When('I press {string}') do |string|
+  click_button(string)
+end
+
+
+
+
+    
+
